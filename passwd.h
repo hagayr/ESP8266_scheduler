a420b87172a7268a99e02737f4d09185586715f0
Hagay <hrozin@gmail.com>
2019-07-11 22:32:16 +0300

    remove ssid and pass

--- passwd.h
+++ passwd.h
@@ -1,11 +1,4 @@
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "";
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "SSID";
-char pass[] = "Pwd";
-const char* update_username = "firmware user name";
-const char* update_password = "firmware password";
+//OTA username and password
+const char* update_username = "OTA username";
+const char* update_password = "OTA Password";
